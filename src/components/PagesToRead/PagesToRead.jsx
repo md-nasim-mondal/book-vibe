13d59466fd539169105d88825c1fb86ec9af3d39
Nasim <nasim.programmer24.com>
2024-03-26 23:27:00 +0600

    handle error page 404

--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,11 +1,25 @@
+import { Link, useRouteError } from "react-router-dom";
 
+const ErrorPage = () => {
+    const error = useRouteError();
 
-const PagesToRead = () => {
     return (
         <div>
-            <h1 className="text-5xl">Here are the books for reading.</h1>
+            <p>{error.statusText || error.message}</p>
+
+            {error.status === 404 && (
+                <div className="flex flex-col items-center mt-24 lg:mt-72 justify-center space-y-6">
+                    <h3 className="text-4xl lg:text-7xl">Page Not Found</h3>
+
+                    <Link to="/">
+                        <button className="btn text-xl font-bold border border-red-600 rounded-lg text-red-600 py-2 px-4 ">
+                            Back Home
+                        </button>
+                    </Link>
+                </div>
+            )}
         </div>
     );
 };
 
-export default PagesToRead;
+export default ErrorPage;
