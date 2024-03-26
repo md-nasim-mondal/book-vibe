387b27cfa6cd36dadb03dafe851da648b40fb1b4
Nasim <nasim.programmer24.com>
2024-03-26 10:23:31 +0600

    made Navbar like as figma without functionality

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,60 @@
+import { NavLink } from "react-router-dom";
+import '../../App.css';
+const Navbar = () => {
+    const links = (
+        <>
+            <li className="text-[#131313CC] text-lg font-normal">
+                <NavLink to={'/'}>Home</NavLink>
+            </li>
+            <li className="text-[#131313CC] text-lg font-normal">
+                <NavLink to={'/listedBooks'}>Listed Books</NavLink>
+            </li>
+            <li className="text-[#131313CC] text-lg font-normal">
+                <NavLink to={'/pagesToRead'}>Pages to Read</NavLink>
+            </li>
+        </>
+    );
+    return (
+        <div className="navbar bg-base-100 p-0 my-4 md:my-12 font-work-sans">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div
+                        tabIndex={0}
+                        role="button"
+                        className="btn-ghost lg:hidden">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-5 w-5"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor">
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 6h16M4 12h8m-8 6h16"
+                            />
+                        </svg>
+                    </div>
+                    <ul
+                        tabIndex={0}
+                        className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                            {links}
+                        </ul>
+                </div>
+                <a className=" btn-ghost text-xl md:text-3xl text-[#131313] font-bold">Book Vibe</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1 gap-2">
+                    {links}
+                </ul>
+            </div>
+            <div className="navbar-end gap-2 md:gap-4">
+                <a className="btn btn-outline bg-[#23BE0A] text-[#FFFFFF] rounded-lg md:text-lg font-medium">Sign In</a>
+                <a className="btn btn-outline bg-[#59C6D2] text-[#FFFFFF] rounded-lg md:text-lg font-medium">Sign Up</a>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,8 +1,10 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-[92%] md:max-w-[82%] mx-auto">
+            <Navbar></Navbar>
             <Outlet></Outlet>
         </div>
     );
