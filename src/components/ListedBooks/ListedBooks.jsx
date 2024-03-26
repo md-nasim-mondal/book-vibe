389f510b8a60218d4bc9e165c60718f6f1834af6
Nasim <nasim.programmer24.com>
2024-03-26 18:12:52 +0600

    added taost successfully and made two tab for Listed Books

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -1,8 +1,34 @@
-
 const ListedBooks = () => {
     return (
         <div>
-            <h1 className="text-6xl">Here Upcoming Listed Books</h1>
+            <div role="tablist" className="tabs tabs-lifted">
+                <input
+                    type="radio"
+                    name="my_tabs_2"
+                    role="tab"
+                    className="tab"
+                    aria-label="Read Books"
+                    checked
+                />
+                <div
+                    role="tabpanel"
+                    className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                    Tab content 1
+                </div>
+
+                <input
+                    type="radio"
+                    name="my_tabs_2"
+                    role="tab"
+                    className="tab"
+                    aria-label="Wishlist Books"
+                />
+                <div
+                    role="tabpanel"
+                    className="tab-content bg-base-100 border-base-300 rounded-box p-6">
+                    Tab content 2
+                </div>
+            </div>
         </div>
     );
 };
